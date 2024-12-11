cfb59da2995c23451516b07f69392d0c46edf35a
RaminBgrn <ramin.bagheriian@gmail.com>
2024-12-11 19:01:10 +0330

    auth controller has been finish start with api route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -24,9 +26,54 @@ class AuthController extends Controller
         );
     }
 
-    public function login() {}
+    private function isValidCredential(LoginRequest $request)
+    {
+        $data = $request->validate();
+        $user = User::where('email', $data['email'])->first();
+        if ($user === null) {
+            return [
+                'success' => false,
+                'message' => 'Invalid Credential'
+            ];
+        }
 
-    public function loginWithToken() {}
+        if (!Hash::check($data['password'], $user->password)) {
+            return [
+                'success' => false,
+                'message' => 'Password is not matched'
+            ];
+        }
+        return [
+            'success' => true,
+            'user' => $user
+        ];
+    }
 
-    public function logout() {}
+    public function login(LoginRequest $request)
+    {
+        $isValid = $this->isValidCredential($request);
+        if (!$isValid['success']) {
+            return $this->error($isValid['message'], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $user = $isValid['user'];
+        $token = $user->createToken(User::USER_TOKEN);
+        return $this->success(
+            [
+                'user' => $user,
+                'token' => $token->plainTextToken,
+            ],
+            "Login successfully"
+        );
+    }
+
+    public function loginWithToken()
+    {
+        return $this->success(auth()->user(), 'Login successfully');
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return $this->success(null, 'Logout successfully');
+    }
 }
