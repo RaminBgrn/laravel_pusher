0155687b20a45d5ab15635e4a62751a3b8e29305
RaminBgrn <ramin.bagheriian@gmail.com>
2024-12-11 19:09:57 +0330

    register login route has been created'

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->as('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('login_with_token', [AuthController::class, 'loginWithToken'])->middleware('auth:sanctum')->name('login_with_token');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
